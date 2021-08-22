519d9f7a04a343f48a4cc9dfdbe8ed6515acaf30
gabysal99 <88295793+gabysal99@users.noreply.github.com>
2021-08-22 15:02:23 -0300

    Entrega 1
    Edicion de archivos products.js y products.html con el fin de realizar lo pedido en la entrega 1

--- js/products.js
+++ js/products.js
@@ -1,6 +1,38 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+var autosArray = [];
 
+function showCategoriesList(array){
+    let mostrarAutos = "";
+
+    for(let i = 0; i < array.length; i++){ 
+        let auto = array[i];
+        mostrarAutos += `
+
+        <div>
+            <img src="` + auto.imgSrc + `" alt="` + auto.description + `" class="img-thumbnail">
+        </div>
+        <div>
+            <h4>`+ auto.name +`</h4> 
+            <p> `+ auto.description +`</p>
+            <p> `+ auto.currency + auto.cost +`</p>
+            <p> `+ auto.soldCount + ` artículos<p>
+            </div>
+        </div>
+        `
+        document.getElementById("productos").innerHTML = mostrarAutos; 
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            autosArray = resultObj.data;
+            showCategoriesList(autosArray);
+        }
+    });
 });
